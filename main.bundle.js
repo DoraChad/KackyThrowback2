ea598d2774764f0305c3ac2be5ffac341fcc6348
DoraChad <176503791+DoraChad@users.noreply.github.com>
2026-09-20 13:29:20 -0700

    fix2

--- main.bundle.js
+++ main.bundle.js
@@ -176,7 +176,7 @@ styles.textContent = `
 document.head.appendChild(styles);
 
 (async () => {
-  const trackData = await fetch('./trackData.json').then(r => r.json());
+  const trackData = await fetch('./resources/trackData.json').then(r => r.json());
   trackData.forEach(t => {
     t.trackMetadata.lastModified = new Date(t.trackMetadata.lastModified);
   });
